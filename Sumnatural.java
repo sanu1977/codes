24dea56370ae5ebe6ce54a9fc06090e4c9a9d9b3
sanu1977 <saniyadesai0106@gmail.com>
2026-08-10 21:34:05 +0530

    Add files via upload

---
+++ Sumnatural.java
@@ -0,0 +1,17 @@
+/* print the sum of first n natural numbers*/
+
+import java.util.*;
+class Sumnatural
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=0;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
