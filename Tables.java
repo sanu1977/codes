1afd75dbd6c07ca57ed28a3ad25c48592559137f
sanu1977 <saniyadesai0106@gmail.com>
2026-08-13 21:43:45 +0530

    Add files via upload

---
+++ Tables.java
@@ -0,0 +1,18 @@
+/*print table of a number given input by user*/
+
+import java.util.*;
+class Tables
+{
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+
+		for(int i=1;i<11;i++)
+		{
+			System.out.println(n +"*" + i +"=" +(n*i));
+		}
+	}
+}
+		
