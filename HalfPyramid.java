6476888186e1eed4c9c3d29e4da191d8d77885ee
sanu1977 <saniyadesai0106@gmail.com>
2026-08-16 20:58:30 +0530

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,24 @@
+/* print half pyramid
+   *
+   **
+   ***
+   ****
+
+*/
+ 
+import java.util.*;
+class HalfPyramid
+{
+	public static void main(String[]args)
+	{
+		int n=4;
+ 		for(int i=1;i<=n;i++)    //outer loop
+		{
+			for(int j=1;j<=i;j++)   //inner loop
+		       {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
