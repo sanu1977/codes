20311485abb1be1f8273258c570f6f1f674b54e0
sanu1977 <saniyadesai0106@gmail.com>
2026-08-16 20:59:25 +0530

    Add files via upload

---
+++ InvertedHpy.java
@@ -0,0 +1,23 @@
+/* print inverted half pyramid
+   ****
+   ***
+   **
+   *
+*/
+
+import java.util.*;
+class InvertedHpy
+{
+	public static void main(String[]args)
+	{
+		int n=4;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
