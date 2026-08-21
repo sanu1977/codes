87965b6839677ef2fa1c26be0cdad4121636888e
sanu1977 <saniyadesai0106@gmail.com>
2026-08-21 22:02:30 +0530

    Add files via upload

---
+++ numberpyramid.java
@@ -0,0 +1,31 @@
+/* print number pyramid
+		1
+	      2   2
+            3   3   3
+         4    4   4   4
+      5     5   5   5    5
+
+*/
+
+
+class numberpyramid
+{
+	public static void main(String[]args)
+	{
+		int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			//space
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			//numbers-->print row no,row no times
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
